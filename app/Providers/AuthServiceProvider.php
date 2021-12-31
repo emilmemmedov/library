53e6b1b76d74591e661f4f847ddc2eabd62214f5
Emil Məmmədov <emill.memmedovv@gmail.com>
2021-12-31 17:57:38 +0400

    done for task

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Librarian;
 use App\Models\User;
-use App\Policies\ModulePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -16,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Librarian::class => ModulePolicy::class
+        //
     ];
 
     /**
